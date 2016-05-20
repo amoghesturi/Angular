198d775537f444d2eb652659a7b3d070efbb6111
Amogh <amoghesturi@gmail.com>
2016-05-20 11:17:37 -0400

    new additions

---
+++ Thinkster/Promises/app.js
@@ -0,0 +1,28 @@
+var app = angular.module('app', []);
+
+app.factory('getData', getData) 
+.run(function(getData) {
+	var promise = getData().
+	then(function(num) {
+		console.log(num);
+		return num * 2 ;
+	})
+	.then(function(num) {
+		console.log(num);
+	})
+	.finally(function () {		
+		console.log('Finished at: ' + new Date());
+	});
+});
+
+function getData($timeout, $q) {
+	return function() {
+		
+		return $q(function(resolve, reject) {
+			$timeout( function() {
+				resolve(Math.floor(Math.random() * 10));
+			}, 2000);
+		});
+		
+	}
+}
