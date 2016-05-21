16dc8b5d3fe27185650b92943e43646348a40ee9
Amoghavarsha Esturi <amoghesturi@gmail.com>
2016-05-21 00:43:32 -0400

    Added bootstrap for table in Filters Reading input from node server

--- app/app.js
+++ app/app.js
@@ -6,13 +6,8 @@ var port = 3000;
 
 var server = http.createServer(function (request, response) {
 
-  // response.header('Access-Control-Allow-Origin', '*');
-  // response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  // response.header('Access-Control-Allow-Headers', 'Content-Type');
-  // response.header('Access-Control-Allow-Credentials', 'true');
-  // response.write(fs.readFileSync("../test.json"));
-  // response.end();
-
-
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.write(fs.readFileSync("../test.json"));
+  response.end();
 
 }).listen(port);
